b5e70172e01c1dadd2f665f6c1288c5f4367da2e
Pedro Tortello <ptortello@hotmail.com>
2023-10-19 21:11:03 -0300

    Changes images path

--- src/constants/initialValues.ts
+++ src/constants/initialValues.ts
@@ -1,61 +1,61 @@
 export const HABITAT: IScore[] = [
   {
     count: 0,
-    image: "/mountain.webp",
+    image: "assets/images/mountain.webp",
     label: "Mountain",
   },
   {
     count: 0,
-    image: "/forest.webp",
+    image: "assets/images/forest.webp",
     label: "Forest",
   },
   {
     count: 0,
-    image: "/prairie.webp",
+    image: "assets/images/prairie.webp",
     label: "Prairie",
   },
   {
     count: 0,
-    image: "/wetland.webp",
+    image: "assets/images/wetland.webp",
     label: "Wetland",
   },
   {
     count: 0,
-    image: "/river.webp",
+    image: "assets/images/river.webp",
     label: "River",
   },
 ];
 
 export const TOKENS: IScore = {
   count: 0,
-  image: "/tokens.webp",
+  image: "assets/images/tokens.webp",
   label: "Tokens",
 };
 
 export const WILDLIFE: IScore[] = [
   {
     count: 0,
-    image: "/bear.webp",
+    image: "assets/images/bear.webp",
     label: "Bear",
   },
   {
     count: 0,
-    image: "/elk.webp",
+    image: "assets/images/elk.webp",
     label: "Elk",
   },
   {
     count: 0,
-    image: "/salmon.webp",
+    image: "assets/images/salmon.webp",
     label: "Salmon",
   },
   {
     count: 0,
-    image: "/hawk.webp",
+    image: "assets/images/hawk.webp",
     label: "Hawk",
   },
   {
     count: 0,
-    image: "/fox.webp",
+    image: "assets/images/fox.webp",
     label: "Fox",
   },
 ];
